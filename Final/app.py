bae3bf25ff694a85a2bb190e253677d553247830
Christy Jestin <cjj9000@gmail.com>
2021-01-17 12:35:36 -0500

    adding Final folder; includes starter app code

---
+++ Final/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify, redirect
+import functions as f
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/search', methods=['POST', 'GET'])
+def redirect_to_display():
+    query=request.form.get("query")
+    query=query.split()
+    query="%20".join(query)
+    return redirect("/search/"+query)
+
+@app.route('/search/<query>', methods=['POST', 'GET'])
+def show_results(query):
+    query = query.split("%20")
+    query = " ".join(query)
+    results = f.add_sentiments(f.full_results(f.search_url(query)), False)
+    return render_template("display.html", results = results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
