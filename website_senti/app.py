9ac3ac79adceb94ac64c58b1240ddb193adf8025
golfcm6 <golfcm6@gmail.com>
2021-01-17 13:27:28 -0500

    final web

--- website_senti/app.py
+++ website_senti/app.py
@@ -14,9 +14,9 @@ def index():
 
 @app.route('/display', methods=['POST'])
 def display():
-    ticker = request.form.get("stock_inquiry")
-    print(ticker)
-    test_list = [{'image': 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20190503-delish-pineapple-baked-salmon-horizontal-ehg-450-1557771120.jpg?crop=0.669xw:1.00xh;0.173xw,0&resize=640:*',
+    inquiry = request.form.get("inquiry")
+    print(inquiry)
+    query_results = [{'image': 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20190503-delish-pineapple-baked-salmon-horizontal-ehg-450-1557771120.jpg?crop=0.669xw:1.00xh;0.173xw,0&resize=640:*',
  'url': 'https://www.bloomberg.com/news/articles/2021-01-14/citroen-sm-tesla-roadster-moke-ugly-cars-that-are-fun-to-drive',
  'title': "check check",
  'polarity': 0.696969,
@@ -25,8 +25,8 @@ def display():
  'title': "check check",
  'polarity': 0.696969,
  'subjectivity': 4.20}]
-    print(test_list[0]['polarity'])
-    return render_template("display.html", ticker=ticker, test_list=test_list)
+    print(query_results[0]['polarity'])
+    return render_template("display.html", inquiry=inquiry, query_results=query_results)
 
 @app.route('/trends', methods=['GET'])
 def trends():
