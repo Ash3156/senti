c6f50c1ab980698b0263f065b37e29c7399e6d80
golfcm6 <golfcm6@gmail.com>
2021-01-17 19:20:25 -0500

    added graph to final

--- Final/app.py
+++ Final/app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request, jsonify, redirect
+
+from flask_cors import CORS, cross_origin
+from random import seed
+from random import random
+
 import functions as f
 
 app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"
+
+]
 
 @app.route('/')
 def index():
@@ -21,9 +36,28 @@ def show_results(query):
     query_results = f.add_sentiments(f.full_results(f.search_url(query)), False)
     return render_template("display.html", query_results = query_results)
 
+
+
+# for the global data charts
+@app.route('/api/data')
+@cross_origin()
+def data():
+    values = []
+    for i in range(8):
+        values.append([])
+        values[i].append([])
+        values[i].append([])
+        for _ in range(6):
+            values[i][0].append(1*random())
+            values[i][1].append(1*random())
+    labels = [6,5,4,3,2,1]
+    _max = 1.2
+
+    return jsonify({'labels': [str(i) for i in labels], 'max': _max, 'values': values})
+
 @app.route('/trends', methods=['GET'])
-def trends():
-    return render_template("trends.html")
+def line():
+    return render_template('trends.html', title='Polarity and Subjectivity By Country')
 
 if __name__ == '__main__':
     app.run(debug=True)
